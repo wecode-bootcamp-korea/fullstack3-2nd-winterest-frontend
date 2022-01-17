440aad3c5165071500cb5bf0382791a1b681e61c
minsukang <minsu910725@gmail.com>
2022-01-17 12:28:09 +0900

    weterest 프론트엔드 초기 세팅완료

---
+++ weterest/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyle from '../src/Styles/GlobalStyle';
+import theme from '../src/Styles/theme';
+import { ThemeProvider } from 'styled-components';
+import Router from './Router';
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Router />
+    </ThemeProvider>
+	</>,
+  document.getElementById('root')
+);
