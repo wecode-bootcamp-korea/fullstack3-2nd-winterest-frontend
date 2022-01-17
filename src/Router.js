bd87922eb99f0727aae3b9709143ef7e6ae21097
minsukang <minsu910725@gmail.com>
2022-01-17 18:56:01 +0900

    Modify: Router 따옴표 수정

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import App from "./App";
+import App from './App';
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default Router;
