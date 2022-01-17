24c0d6440664b2e7af949e6367ef2c81c8af528c
minsukang <minsu910725@gmail.com>
2022-01-17 18:48:31 +0900

    Modify: config 파일 삭제 및 프리티어 수정

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GlobalStyle from "../src/Styles/GlobalStyle";
-import Theme from "./Styles/Theme";
-import { ThemeProvider } from "styled-components";
-import Router from "./Router";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyle from '../src/Styles/GlobalStyle';
+import Theme from './Styles/Theme';
+import { ThemeProvider } from 'styled-components';
+import Router from './Router';
 
 ReactDOM.render(
-  <>
-    <GlobalStyle />
-    <ThemeProvider Theme={Theme}>
-      <Router />
-    </ThemeProvider>
-  </>,
-  document.getElementById("root")
+	<>
+		<GlobalStyle />
+		<ThemeProvider Theme={Theme}>
+			<Router />
+		</ThemeProvider>
+	</>,
+	document.getElementById('root'),
 );
