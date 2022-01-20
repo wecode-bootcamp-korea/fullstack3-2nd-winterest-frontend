db18cb003bc23b62b2145cb5df76d09d5093b15d
TaeJoonPark <tjpark1028@gmail.com>
2022-01-20 18:21:09 +0900

    수정: git rebase develop branch

---
+++ src/Pages/User/ImgUpload.js
@@ -0,0 +1,320 @@
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+import styled, { css } from 'styled-components';
+import { BiDotsHorizontalRounded } from 'react-icons/bi';
+import { HiArrowCircleUp } from 'react-icons/hi';
+import { FaTrash } from 'react-icons/fa';
+
+const ImgUpload = () => {
+  //useState 관리
+  const [tag, setTag] = useState('');
+  const [imageUrl, setImageUrl] = useState(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imageAlt, setImageAlt] = useState('');
+  const [titleArea, setTitleArea] = useState('');
+  const [error, setError] = useState(false);
+  const [fileName, setFileName] = useState('');
+
+  //useRef 변수에 할당
+  const ref = React.useRef();
+
+  //Title textarea 자동 줄추가 및 스크롤 제거
+  const titleAreaResize = e => {
+    ref.current.style.height = '46px';
+    ref.current.style.height = ref.current.scrollHeight + 'px';
+    setTitle(e.target.value);
+    console.log('Title:', title);
+  };
+
+  const descriptionAreaResize = e => {
+    ref.current.style.height = '23px';
+    ref.current.style.height = ref.current.scrollHeight + 'px';
+    setDescription(e.target.value);
+    console.log('Description:', description);
+  };
+
+  const previewHandler = e => {
+    const selected = e.target.files[0];
+    const ALLOWED_TYPES = ['image/png', 'image/jpeg', 'image/png'];
+    if (selected && ALLOWED_TYPES.includes(selected.type)) {
+      setFileName(selected);
+      //FileReader Method
+      // let reader = new FileReader();
+      // reader.onloadend = () => {
+      //   setImageUrl(reader.result.toString());
+      // };
+      // reader.readAsDataURL(selected); //파일을 읽어 버퍼에 저장
+      // setImageUrl(selected);
+      // URL Method
+      setImageUrl(URL.createObjectURL(selected));
+    } else {
+      console.log('Not Supported File.');
+      setError(true);
+    }
+  };
+
+  // URL 미리보기 삭제(메모리관리)
+  const delPreviewHandler = () => {
+    URL.revokeObjectURL(imageUrl);
+    setImageUrl(null);
+  };
+
+  const tagHandler = e => {
+    setTag(e.target.value);
+    console.log('Tag: ', tag);
+  };
+
+  const postHandler = () => {
+    console.log('imageUrl: ', fileName);
+    const datas = {
+      tag: tag,
+      title: title,
+      desc: description,
+    };
+    const formData = new FormData();
+    formData.append('file', fileName);
+    formData.append(
+      'datas',
+      datas,
+      // new Blob([JSON.stringify(datas)], { type: 'application/json' }),
+    );
+    // formData.append('title', title);
+    // formData.append('description', description);
+    for (let key of formData.keys()) {
+      console.log('formData key: ', key);
+    }
+    for (let value of formData.values()) {
+      console.log('formData value: ', value);
+    }
+    axios
+      .post(`${process.env}`, formData, {
+        onUploadProgress: progressEvent => {
+          console.log(
+            'Upload Progress: ' +
+              Math.round((progressEvent.loaded / progressEvent.total) * 100) +
+              '%',
+          );
+        },
+      })
+      .then(res => {
+        console.log(res);
+      });
+
+    return axios;
+  };
+
+  return (
+    <Section background="lightgrey">
+      <UploadBox>
+        <InnerBox>
+          <OptionAndTag>
+            <BiDotsHorizontalRounded
+              className="dotThree"
+              onClick={() => alert('아직 준비중입니다!')}
+            />
+            <section>
+              <input type="text" onChange={tagHandler}></input>
+              <SaveButton onClick={postHandler}>저장</SaveButton>
+            </section>
+          </OptionAndTag>
+          <PreviewAndInfo>
+            <Preview>
+              {imageUrl ? (
+                <>
+                  <DelIcon onClick={delPreviewHandler}>
+                    {/* FileReader Method */}
+                    {/* <FaTrash
+                      className="trash"
+                      onClick={() => setImageUrl(null)}
+                    /> */}
+                    {/* URL Method */}
+                    <FaTrash className="trash" />
+                  </DelIcon>
+                  <PreviewImg alt="sample" url={imageUrl} />
+                </>
+              ) : (
+                <label htmlFor="input-file">
+                  <BorderDashed>
+                    <HiArrowCircleUp className="arrowUp" />
+                    <p>클릭하여 이미지 업로드</p>
+                  </BorderDashed>
+                </label>
+              )}
+              <input id="input-file" type="file" onChange={previewHandler} />
+            </Preview>
+            <Info>
+              <TextTitle
+                ref={ref}
+                onInput={titleAreaResize}
+                placeholder="제목 추가"
+              />
+              <UserName>
+                <CircleUserWord>T</CircleUserWord>
+                &nbsp;&nbsp;TjPark
+              </UserName>
+              <TextDescription
+                ref={ref}
+                onInput={descriptionAreaResize}
+                placeholder="사람들에게 회원님의 win에 대해 설명해 보세요"
+              />
+            </Info>
+          </PreviewAndInfo>
+        </InnerBox>
+      </UploadBox>
+    </Section>
+  );
+};
+
+//전역 styled-component
+const TextArea = css`
+  width: 100%;
+  resize: none;
+  border: 0;
+  border-bottom: 1px solid grey;
+  outline: none;
+  overflow: auto;
+  word-spacing: -5px;
+`;
+
+const ImgArea = css`
+  width: 340px;
+  height: 454px;
+  border-radius: 1rem;
+  background-color: rgb(237, 237, 237);
+`;
+
+const Section = styled.section`
+  background-color: rgb(237, 237, 237);
+`;
+
+const UploadBox = styled.section`
+  width: 880px;
+  height: 650px;
+  margin: 100px auto;
+  border-radius: 1rem;
+  background-color: ${props => props.theme.white};
+`;
+
+const InnerBox = styled.section`
+  padding: 40px 40px 0 40px;
+`;
+
+const OptionAndTag = styled.section`
+  display: flex;
+  justify-content: space-between;
+  height: 40px;
+  margin-left: 40px;
+
+  .dotThree {
+    color: grey;
+    font-size: 1.7rem;
+  }
+`;
+
+const PreviewAndInfo = styled.section`
+  display: flex;
+  flex-direction: row;
+  margin-top: 10px;
+`;
+
+const Preview = styled.section.attrs(props => ({}))`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  ${ImgArea};
+
+  input {
+    display: none;
+  }
+`;
+
+const DelIcon = styled.section`
+  position: absolute;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  margin-right: 17rem;
+  background-color: white;
+  padding: 15px;
+  .trash {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
+const PreviewImg = styled.img.attrs(props => ({
+  alt: props.alt,
+  src: props.url,
+}))`
+  ${ImgArea};
+`;
+
+const BorderDashed = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 310px;
+  height: 424px;
+  margin: 10px;
+  border: dashed 2px lightgrey;
+  border-radius: 1rem;
+
+  .arrowUp {
+    color: gray;
+    font-size: 2.5rem;
+  }
+
+  p {
+    margin-top: 20px;
+  }
+`;
+
+const Info = styled.section`
+  width: 380px;
+  margin-left: 20px;
+`;
+
+const TextTitle = styled.textarea.attrs(props => ({
+  placeholder: props.placeholder,
+}))`
+  height: 46px;
+  margin-top: 40px;
+  font-size: 36px;
+  font-weight: 700;
+  ${TextArea}
+`;
+
+const TextDescription = styled.textarea.attrs(props => ({
+  placeholder: props.placeholder,
+}))`
+  height: 23px;
+  margin-top: 40px;
+  font-size: 18px;
+  ${TextArea}
+`;
+
+const UserName = styled.section`
+  display: flex;
+
+  margin-top: 40px;
+  height: 45px;
+  line-height: 45px;
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+`;
+
+const CircleUserWord = styled.section`
+  width: 45px;
+  border-radius: 50%;
+  font-size: 20px;
+  font-weight: 600;
+  background-color: rgb(237, 237, 237);
+`;
+
+const SaveButton = styled.button``;
+
+export default ImgUpload;
--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from '../src/Pages/Main/Main';
+import WinDetail from './Pages/Detail/WinDetail';
+import ImgUpload from './Pages/User/ImgUpload';
+
+import App from './App';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/" element={<App />} />
+        <Route path="/win/winid" element={<WinDetail />} />
+        <Route path="/win/user/upload" element={<ImgUpload />} />
       </Routes>
     </BrowserRouter>
   );
